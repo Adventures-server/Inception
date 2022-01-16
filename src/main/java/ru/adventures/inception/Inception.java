5f6765cf7c439eb6a0c1eb9f8490c0cbf4193d06
escanorus <korabelnikov.core@gmail.com>
2022-01-16 17:56:59 +0300

    Initial commit

---
+++ src/main/java/ru/adventures/inception/Inception.java
@@ -0,0 +1,16 @@
+package ru.adventures.inception;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Inception extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
